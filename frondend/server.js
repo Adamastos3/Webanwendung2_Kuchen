0886209b9a5d53f3bdd74b95babd6dd81f656a45
Adamastos3 <chseifriz3@gmail.com>
2021-03-23 09:45:48 +0100

    Server init with three routes

---
+++ frondend/server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express=require('express');
+
+
+//Einstellungen
+server.use(express.urlencoded({ extended: false }))
+
+
+//middleware
+//uebergabe static files
+server.use(express.static(path.join(__dirname,'public')));
+
+//Routen
+//Index
+server.get('/', (req,res) =>{
+    res.sendFile("index.html", {root: path.join(__dirname, 'view')});
+});
+
+//Register
+server.get("/register", (req,res) =>{
+    res.sendFile('register.html', { root: path.join(__dirname, 'view') });
+});
+
+//login
+server.get("/login", (req,res) =>{
+    res.sendFile('login.html', { root: path.join(__dirname, 'view') });
+});

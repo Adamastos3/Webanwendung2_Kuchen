931fb1a28ab546cb5a677e0c9c250d62febdd7ad
Adamastos3 <chseifriz3@gmail.com>
2021-04-04 19:36:17 +0200

    init js for kontakt.html

---
+++ frondend/public/js/kontakt.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form")
+
+//prototyp
+form.addEventListener("submit", (e)=>{
+    
+    e.preventDefault();
+    window.alert("Danke, dass Sie uns kontaktiert haben")
+    
+    location.href="/start"
+})
+
+
+

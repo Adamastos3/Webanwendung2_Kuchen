ee9fa88a5baef2eed7f66edbdc7b17a49c46fccc
Adamastos3 <chseifriz3@gmail.com>
2021-04-04 19:38:00 +0200

    delete init seesionstorage

--- frondend/public/js/sortimentI.js
+++ frondend/public/js/sortimentI.js
@@ -2,12 +2,7 @@ const form = document.getElementById("form")
 
 var myVar = setInterval(addSum, 1000);
 
-//init seesionstorage
-if (sessionStorage.length==0) {
-    sessionStorage.setItem( "regular",null)
-    sessionStorage.setItem("Individual", null)
-    console.log("test");
-}
+
 
 function addSum() {
     let summ= document.getElementById("summe")
@@ -58,38 +53,3 @@ form.addEventListener("submit", (e)=>{
 })
 
 
-function storeAnzeigen() {
-    let anzahlR=0
-    let anzahlI=0
-    let w = document.getElementById("zahl");
-    let r = sessionStorage.getItem("regular")
-    let i= sessionStorage.getItem("individual");
-    try {
-        console.log(r[0])
-        if (r[0]!="n"){
-            console.log("t1")
-            anzahlR+=1
-        }
-        anzahlR+=r.split(",").length-1
-        
-    }
-    catch {
-       
-    }
-
-    try {
-        if (i[0]!="n"){
-            anzahlI+=1
-        }
-        anzahlI+=i.split(",").length-1
-        
-    }
-    catch {
-       
-    }
-    
-    let anzahl = anzahlI+anzahlR;
-    if (anzahl >0) {
-        w.innerHTML=" ("+anzahl+")"
-    }
-}

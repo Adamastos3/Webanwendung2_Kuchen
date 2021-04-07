c8504b8e4d997e98eaa548471c319a454b127a43
Adamastos3 <chseifriz3@gmail.com>
2021-04-07 23:45:19 +0200

    add admin login

--- frondend/public/js/login.js
+++ frondend/public/js/login.js
@@ -1,15 +1,33 @@
 
 
 //prototyp
-document.addEventListener("keyup", (e)=>{
+document.addEventListener("keydown", (e)=>{
     
 
     if(e.keyCode===13){
+        console.log("tetetet")
 
         e.preventDefault();
-        sessionStorage.setItem("login", 1)
+        var a= document.getElementById("username").value;
+        if(a=="admin"){
+            if(document.getElementById("pass").value="admin"){
+                sessionStorage.setItem("admin",1);
+                location.href="accountAdmin.html";
+            }
+            else{
+                alert("Falsche Eingaben. Bitte versuchen Sie es nochmal")
+                document.forms.form.reset();
+
+            }
+        }
+        else{
+            sessionStorage.setItem("login", 1)
+            console.log("ende")
+            location.href="account.html"
+        }
+        
     
-        location.href="account.html"
+        
     };
   
 })

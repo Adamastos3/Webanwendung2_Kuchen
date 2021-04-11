43fb8ccad5f3c6b04b180950fd8a7a19e75730d8
Adamastos3 <chseifriz3@gmail.com>
2021-04-11 20:23:36 +0200

    bug fix radion change

--- frondend/public/js/kontakt.js
+++ frondend/public/js/kontakt.js
@@ -4,10 +4,24 @@ const form = document.getElementById("form")
 form.addEventListener("submit", (e)=>{
     
     e.preventDefault();
+    sendOn()
+})
+
+function changeRadion(a){
+    if (a =="Herr") {
+        document.getElementById("Frau").checked=false;
+        document.getElementById("Herr").checked=true;
+    }
+    else{
+        document.getElementById("Frau").checked=true;
+        document.getElementById("Herr").checked=false;
+    }
+}
+
+function sendOn(){
     window.alert("Danke, dass Sie uns kontaktiert haben")
     
     location.href="start.html"
-})
-
+}
 
 

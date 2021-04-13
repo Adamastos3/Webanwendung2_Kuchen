fefc722799f1334b8d50e0cbf7edc5355018e9d4
Adamastos3 <chseifriz3@gmail.com>
2021-04-13 15:39:10 +0200

    bug fix addsum

--- frondend/public/js/warenkorb.js
+++ frondend/public/js/warenkorb.js
@@ -22,8 +22,9 @@ function addsumm() {
     let wert=0
     for (let i=0; i< a.length; i++){
         let d=a[i].innerHTML;
+        console.log("inner")
         console.log(d)
-        d= d.substr(-4,3)
+        d= d.substr(-3,2)
         console.log(d)
         wert=wert+Number(d)
     }

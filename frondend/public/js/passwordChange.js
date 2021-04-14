48d2a60d788b8c3779cac93537ebcf39a7bf4f6f
Adamastos3 <chseifriz3@gmail.com>
2021-04-14 10:30:26 +0200

    add password check

--- frondend/public/js/passwordChange.js
+++ frondend/public/js/passwordChange.js
@@ -1,7 +1,7 @@
-const form = document.getElementById("form")
+const forme = document.getElementById("form")
 
 //prototyp
-form.addEventListener("submit", (e)=>{
+forme.addEventListener("submit", (e)=>{
     
     e.preventDefault();
     submitten()
@@ -10,7 +10,32 @@ form.addEventListener("submit", (e)=>{
 function submitten(){
     sessionStorage.setItem("login", 0)
     
-    location.href="login.html"
+    //console.log(checkPassword())
+
+    if(checkPassword()){
+        location.href="login.html"
+    }
+}
+
+function checkPassword(){
+    let a = document.getElementById("passwordNew")
+    let b = document.getElementById("passwordNew2")
+
+    if (a.value.length <8){
+        window.alert("Passwort ist zu kurz (min. 8 Zeichen)")
+        document.forms.form.reset()
+        return false
+    }
+    else{
+        if (a.value == b.value){
+            return true
+        }
+        else{
+            window.alert("Die Passwörter sind nicht gleich")
+            document.forms.form.reset()
+            return false
+        }
+    }
 }
 
 

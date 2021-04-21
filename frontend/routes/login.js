f5c649b0663b6ca714fdc3e9a7065854e423bef0
Adamastos3 <chseifriz3@gmail.com>
2021-04-21 10:01:23 +0200

    add function to login

--- frontend/routes/login.js
+++ frontend/routes/login.js
@@ -1,7 +1,8 @@
 const path= require('path')
 const express = require("express");
 const server =express()
-
+const login= require("../API_Access/Login/login")
+const session = require("express-session")
 
 //login
 server.get("/login", (req,res) =>{
@@ -9,9 +10,27 @@ server.get("/login", (req,res) =>{
 });
 
 server.post('/login', (req,res)=>{
-    
+    console.log("login")
+    logins(req,res)
 })
 
+
+async function logins(req,res){
+    const b= await login(req.body);
+    console.log("Test")
+    console.log(b)
+    if (b == undefined){
+        res.redirect("/login")
+    }
+    else{
+        console.log("angemeldet")
+        req.session.isAuth=true
+        req.session.username=b;
+        res.redirect("/account")
+
+    }
+
+}
 
 
 module.exports=server;

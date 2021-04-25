736682373f2770167051a2202e4896f41fbdb337
Adamastos3 <chseifriz3@gmail.com>
2021-04-25 16:47:34 +0200

    bug fix

--- frontend/routes/login.js
+++ frontend/routes/login.js
@@ -2,7 +2,7 @@ const path= require('path')
 const express = require("express");
 const server =express()
 const login= require("../API_Access/Login/login")
-const session = require("express-session")
+//const session = require("express-session")
 
 //login
 server.get("/login", (req,res) =>{
@@ -16,34 +16,39 @@ server.post('/login', (req,res)=>{
 
 
 async function logins(req,res){
-    const val="";
+    var val="";
     const name="KN"
     const b= await login(req.body);
     console.log("Test")
     console.log(b)
-    if (b == undefined){
+    if (b == 0){
         res.redirect("/login")
     }
     else{
         console.log("angemeldet")
+        //console.log(typeof(b))
         req.session.isAuth=true
-        req.session.username=b;
+        req.session.username=b.id;
 
         if(b >1000){
-            val=b;
+            val=b.id;
         }
         else if (b>100){
-            val ="0"+b
+            val ="0"+b.id
         }
         else if (b > 10){
-            val="00"+b
+            val="00"+b.id
         }
         else{
-            val="000"+b
+            val="000"+b.id
         }
 
         res.cookie(name, val, {maxAge: 1000*60*60*24});
-        res.redirect("/account")
+        if(b.benutzerrolle.id ==1){
+            res.redirect("/accountAdmin")
+        }else{
+            res.redirect("/account")
+        }
 
     }
 

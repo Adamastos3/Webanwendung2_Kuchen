58c1b56a4c94403013d143b8c9196b81cf649513
VladoSwiderski <vlado.swid@gmail.com>
2021-04-25 16:12:54 +0200

    css/js for sortimentRNew

---
+++ frondend/public/js/replaceDiv.js
@@ -0,0 +1,55 @@
+function replaceDiv1() {
+document.getElementById("DivBefore1").style.display="none";
+document.getElementById("DivAfter1").style.display="block";
+
+
+
+}
+
+function replaceDiv1Invert() {
+document.getElementById("DivBefore1").style.display="block";
+document.getElementById("DivAfter1").style.display="none";
+}
+
+
+
+
+
+function replaceDiv2() {
+document.getElementById("DivBefore2").style.display="none";
+document.getElementById("DivAfter2").style.display="block";
+}
+
+function replaceDiv2Invert() {
+document.getElementById("DivBefore2").style.display="block";
+document.getElementById("DivAfter2").style.display="none";
+}
+
+
+
+
+
+function replaceDiv3() {
+document.getElementById("DivBefore3").style.display="none";
+document.getElementById("DivAfter3").style.display="block";
+}
+
+function replaceDiv3Invert() {
+document.getElementById("DivBefore3").style.display="block";
+document.getElementById("DivAfter3").style.display="none";
+}
+
+
+
+
+
+
+function replaceDiv4() {
+document.getElementById("DivBefore4").style.display="none";
+document.getElementById("DivAfter4").style.display="block";
+}
+
+function replaceDiv4Invert() {
+document.getElementById("DivBefore4").style.display="block";
+document.getElementById("DivAfter4").style.display="none";
+}

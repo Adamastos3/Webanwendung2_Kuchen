c169b6d4d42fe5725a84a958c562e288daf82a2a
Adamastos3 <chseifriz3@gmail.com>
2021-04-25 16:45:51 +0200

    add request

--- frontend/public/js/Kundendaten.js
+++ frontend/public/js/Kundendaten.js
@@ -1,8 +1,83 @@
 
-function erledigt(id) {
+var counter=1;
+
+function requestKundenDaten() {
+    var id = 1//cookies()
+
+    if (id > 0) {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://localhost:8000/wba2api/benutzer/alle')
+        request.onload = function () {
+            var data = JSON.parse(request.responseText);
+            console.log("request")
+            console.log(data);
+            if (data.fehler == false) {
+                setzenHtmlKundenDaten(data.daten, id)
+            }
+            else {
+                console.log("error")
+            }
+
+        }
+        request.send();
+    }
+
+}
+function setzenHtmlKundenDaten(data, id) {
+    var table1 = document.getElementById("tableKunden")
+    var text = "";
+
+    for (let i = 0; i < data.length; i++) {
+
+
+        if (data[i].id != id && data[i].benutzerrolle.id >=3) {
+            console.log("ausgabe")
+            let elem="elem"+counter;
+            let ids="ids"+counter
+            //id
+            text += "<td><p id='"+ids+"'>" + data[i].id + "</p></td>";
+
+            let person = data[i].person;
+            if(person != null){
+            text+=  "<td><p> Mail: " + person.email+"</p>"+
+                "Username: "+data[i].benutzername+"</p>"+
+                "<p>Name: "+person.vorname +" "+person.nachname+"</p>"+
+                "<p>Geburtstag: "+person.geburtstag+"</p>"+
+                "<p>Adresse: "+person.adresse.strasse+" "+person.adresse.hausnummer+" "+person.adresse.plz+" "+person.adresse.ort+"</p>"+
+                "</td>";
+        
+            }
+            
+            //Rolle
+            text += "<td><p>" + data[i].benutzerrolle.bezeichnung + "</p></td>";
+            
+            
+            //button
+            text += "<td><button onclick='sendOn()'><img src='./img/pencil.png' alt=''></button></td>"+
+                "<td><button onclick='erledigt("+elem+","+ids+")'><img src='./img/bin.png' alt=''></button></td>";
+            console.log(table1)
+            console.log(text)
+            let rw = table1.insertRow(counter);
+            rw.setAttribute("id", elem)
+            rw.innerHTML += text;
+            console.log(table1)
+            text="";
+            counter++;
+            
+        }
+            
+    }
+
+}
+
+function erledigt(id,idU) {
+    console.log("erledigt")
     console.log(id)
-    let element = document.getElementById(id);
-    //console.log(element)
+    console.log(idU)
+
+    let element = id;
+    let ids= idU.innerHTML;
+    deleteRequest(ids);
     element.parentNode.removeChild(element);
     //Es fehl noch das entfernen aus dem sessionstore
 }
@@ -10,3 +85,25 @@ function erledigt(id) {
 function sendOn(){
     location.href="kundenChange.html"
 }
+
+
+function deleteRequest(id){
+    console.log("ids")
+    console.log(id)
+    var request = new XMLHttpRequest();
+        request.open('DELETE', 'http://localhost:8000/wba2api/benutzer/'+id)
+        request.onload = function () {
+            var data = JSON.parse(request.responseText);
+            console.log("delete")
+            console.log(data);
+            
+
+        }
+        request.send();
+
+}
+
+//aufrufen
+requestKundenDaten();
+
+

557f2c45fe9afee35ebcbd9e4cde7a3ec9207e59
Adamastos3 <chseifriz3@gmail.com>
2021-04-26 15:26:53 +0200

    add post route

--- frontend/routes/ihreDaten.js
+++ frontend/routes/ihreDaten.js
@@ -1,13 +1,25 @@
-const path= require('path')
+const path = require("path");
 const express = require("express");
-const server =express()
-const isAuth=require("../middleware/controller")
+const server = express();
+const isAuth = require("../middleware/controller");
+const ihreDaten = require("../API_Access/IhreDaten/ihreDaten");
 
 //ihreDaten
-server.get("/ihreDaten", isAuth, (req,res) =>{
-    res.sendFile('ihreDaten.html', { root: path.join(__dirname,'..', 'view') });
+server.get("/ihreDaten", isAuth, (req, res) => {
+  res.sendFile("ihreDaten.html", { root: path.join(__dirname, "..", "view") });
 });
 
+server.post("/ihreDaten", isAuth, (req, res) => {
+  setData(req, res);
+});
 
+async function setData(req, res) {
+  const b = await ihreDaten(req.body, req.session.username);
+  if (b != undefined) {
+    res.redirect("/ihreDaten");
+  } else {
+    res.status(404).redirect("/ihreDaten");
+  }
+}
 
-module.exports=server;
+module.exports = server;

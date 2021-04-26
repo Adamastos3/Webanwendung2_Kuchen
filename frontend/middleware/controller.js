49dd0e1f3847c3896add034d393a121287c98acf
Adamastos3 <chseifriz3@gmail.com>
2021-04-26 15:26:29 +0200

    add testlog

--- frontend/middleware/controller.js
+++ frontend/middleware/controller.js
@@ -1,10 +1,10 @@
-
-function isAuth(req,res,next){
-    if(req.session.isAuth){
-        next()
-    }else{
-        res.redirect("/login")
-    }
+function isAuth(req, res, next) {
+  if (req.session.isAuth) {
+    console.log(req.session.username);
+    next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
-module.exports=isAuth
+module.exports = isAuth;

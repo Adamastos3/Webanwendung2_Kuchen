b5fdeb8f5eddf38bc05dbc2a61a739234aabaaaf
Adamastos3 <chseifriz3@gmail.com>
2021-04-26 10:47:40 +0200

    delete requestBestell

--- frontend/public/js/bestellhistorie.js
+++ frontend/public/js/bestellhistorie.js
@@ -1,71 +1,67 @@
-var counter=1;
+var counter = 1;
+const pathBestellHistory = "http://localhost:8000/wba2api/bestellung/alle";
 
-function requestBestell() {
-    var id = 1//cookies()
+function setzenHtmlBestell(data) {
+  let id = 1;
+  console.log(id);
+  var table1 = document.getElementById("tableBestell");
+  var text = "";
+  console.log("daten");
+  console.log(data.length);
+  console.log(data[0].besteller);
 
-    if (id > 0) {
-        var request = new XMLHttpRequest();
-        request.open('GET', 'http://localhost:8000/wba2api/bestellung/alle')
-        request.onload = function () {
-            var data = JSON.parse(request.responseText);
-            console.log("request")
-            console.log(data);
-            if (data.fehler == false) {
-                setzenHtmlBestell(data.daten, id)
-            }
-            else {
-                console.log("error")
-            }
+  for (let i = 0; i < data.length; i++) {
+    //nur zum testen
+    if (data[i].besteller == null) {
+    } else {
+      //normal
+      if (data[i].besteller.id == id) {
+        console.log("ausgabe" + counter);
+        text += "<td><p>" + data[i].id + "</p></td>" + "<td><p>";
 
+        let bestell = data[i].bestellpositionen;
+        for (let j = 0; j < bestell.length; j++) {
+          text +=
+            "" +
+            bestell[j].menge +
+            "x " +
+            bestell[j].produkt.bezeichnung +
+            "<br>" +
+            bestell[j].produkt.beschreibung +
+            "<br> <br>";
         }
-        request.send();
-    }
 
-}
-function setzenHtmlBestell(data, id) {
-    var table1 = document.getElementById("tableBestell")
-    var text = "";
-    console.log("daten")
-    console.log(data.length)
-    console.log(data[0].besteller)
+        //Preis
+        text +=
+          "</p></td>" +
+          "<td><p>" +
+          data[i].total.brutto +
+          " € </p></td>" +
+          "<td><p>" +
+          //bestellzeitpunkt
+          data[i].bestellzeitpunkt.substring(0, 10) +
+          "</p></td>" +
+          "<td><p>";
 
-    for (let i = 0; i < data.length; i++) {
-
-        //nur zum testen
-        if (data[i].besteller == null) {
-
+        //Lieferzeitpunkt
+        if (data[i].lieferzeitpunkt == null) {
+          text += "Nicht geliefert";
         } else {
-            //normal
-            if (data[i].besteller.id == id) {
-                console.log("ausgabe"+counter)
-                text += "<td><p>" + data[i].id + "</p></td>" +
-                    "<td><p>";
-
-                let bestell = data[i].bestellpositionen;
-                for (let j = 0; j < bestell.length; j++) {
-                    text += "" + bestell[j].menge + "x " + bestell[j].produkt.bezeichnung + "<br>"
-                }
-
-                text += "</p></td>" +
-                    "<td><p>" + data[i].total.brutto + " € </p></td>" +
-                    "<td><p>" + data[i].bestellzeitpunkt + "</p></td>"
-                
-                console.log(table1)
-                console.log(text)
-                let rw = table1.insertRow(1);
-                rw.innerHTML += text;
-                console.log(table1)
-                text="";
-                counter++
-            }
-            
+          text += data[i].bestellzeitpunkt.substring(0, 10);
         }
 
-    }
+        text += "</p></td>";
 
+        console.log(table1);
+        console.log(text);
+        let rw = table1.insertRow(1);
+        rw.innerHTML += text;
+        console.log(table1);
+        text = "";
+        counter++;
+      }
+    }
+  }
 }
 
-
-
-//aufrufen
-requestBestell();
+getRequest(pathBestellHistory, setzenHtmlBestell);

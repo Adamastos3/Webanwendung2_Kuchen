9e5f5d9514a56e314203de1f4a750f546e2c9915
Adamastos3 <chseifriz3@gmail.com>
2021-04-26 15:25:53 +0200

    formattet + add update person

--- frontend/API_Access/Person/person.js
+++ frontend/API_Access/Person/person.js
@@ -1,22 +1,29 @@
-const getRequest= require("./../Request/getRequest")
-const postRequest= require("./../Request/postRequest")
+const getRequest = require("./../Request/getRequest");
+const postRequest = require("./../Request/postRequest");
+const putRequest = require("./../Request/putRequest");
 
-async function getPersonbyId(id){
-    let pa="/wba2api/person/gib/"+id;
-    const b = await getRequest(pa);
-    return b
-   
+async function getPersonbyId(id) {
+  let pa = "/wba2api/person/gib/" + id;
+  const b = await getRequest(pa);
+  return b;
 }
 
-async function createPerson(data){
-    console.log("start")
-    console.log(data)
-    let pa="/wba2api/person";
-    const b = await postRequest(pa,data)
-    return b.id;
+async function createPerson(data) {
+  //console.log("start");
+  //console.log(data);
+  let pa = "/wba2api/person";
+  const b = await postRequest(pa, data);
+  return b.id;
 }
 
-module.exports={
-    getPersonbyId,
-    createPerson,
+async function updatePerson(data) {
+  let pa = "/wba2api/person";
+  const b = await putRequest(pa, data);
+  return b.id;
 }
+
+module.exports = {
+  getPersonbyId,
+  createPerson,
+  updatePerson,
+};

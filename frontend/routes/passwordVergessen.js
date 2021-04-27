5a064ca0f92d973c2691acc12e5e10db48ca61e3
Adamastos3 <chseifriz3@gmail.com>
2021-04-27 15:54:18 +0200

    add post route

--- frontend/routes/passwordVergessen.js
+++ frontend/routes/passwordVergessen.js
@@ -1,5 +1,6 @@
 const path= require('path')
 const express = require("express");
+const { getBenutzerAll } = require('../API_Access/Benutzer/benutzer');
 const server =express()
 
 
@@ -8,6 +9,17 @@ server.get("/passwordVergessen", (req,res) =>{
     res.sendFile('passwordVergessen.html', { root: path.join(__dirname,'..', 'view') });
 });
 
+server.post("/passwordVergessen", (req,res) =>{
+    passV(req,res)
+})
 
+async function passV(req,res){
+    const data = getBenutzerAll();
+    for (let i=0; i< data.length; i++){
+        if (data[i].person.email == req.body.email){
+            const pass = null;
+        }
+    }
 
+}
 module.exports=server;

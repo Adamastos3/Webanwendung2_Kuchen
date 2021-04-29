633bc871f0b3c23a554fd9a0483f2ad19a2b3534
Adamastos3 <chseifriz3@gmail.com>
2021-04-29 16:23:16 +0200

    Fixed spelling errors bug fix change cookies

--- frontend/public/js/allgemein.js
+++ frontend/public/js/allgemein.js
@@ -1,4 +1,5 @@
-const pathAllgemein = "http://localhost:8000/wba2api/benutzer/gib/" + cookies();
+const pathAllgemein =
+  "http://localhost:8000/wba2api/benutzer/gib/" + cookies()[0];
 
 //init seesionstorage
 if (sessionStorage.length === 0) {
@@ -8,7 +9,7 @@ if (sessionStorage.length === 0) {
   sessionStorage.setItem("admin", 0);
   sessionStorage.setItem("kasse", 0);
   console.log("init");
-  initBenutzer();
+  //initBenutzer();
 }
 
 getRequest(pathAllgemein, setzenHtml);
@@ -16,39 +17,31 @@ getRequest(pathAllgemein, setzenHtml);
 //Richtig
 //
 function getRequest(path, func) {
-  var id = cookies();
-
-  if (id > 0) {
-    var request = new XMLHttpRequest();
-    request.open("GET", path);
-    request.onload = function () {
-      var data = JSON.parse(request.responseText);
-      console.log(data);
-      if (data.daten != null) {
-        func(data.daten);
-      } else {
-        console.log("error, NO Data");
-      }
-    };
-    request.send();
-  }
+  var request = new XMLHttpRequest();
+  request.open("GET", path);
+  request.onload = function () {
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+    if (data.daten != null) {
+      func(data.daten);
+    } else {
+      console.log("error, NO Data");
+    }
+  };
+  request.send();
 }
 
 function postRequest(path, data) {
-  var id = cookies();
+  var request = new XMLHttpRequest();
+  request.open("POST", path);
+  request.setRequestHeader("Content-type", "application/json");
+  request.setRequestHeader("Content-Length", data.length);
 
-  if (id > 0) {
-    var request = new XMLHttpRequest();
-    request.open("POST", path);
-    request.setRequestHeader("Content-type", "application/json");
-    request.setRequestHeader("Content-Length", data.length);
-
-    request.onload = function () {
-      var data = JSON.parse(request.responseText);
-      console.log(data);
-    };
-    request.send(data);
-  }
+  request.onload = function () {
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+  };
+  request.send(data);
 }
 
 function setzenHtml(data) {
@@ -56,6 +49,7 @@ function setzenHtml(data) {
   let user = document.getElementById("user");
   let shop = document.getElementById("shop");
   let be = data.benutzerrolle.id;
+  console.log(be);
   let usernamen = data.benutzername;
   if (be == 3) {
     user.innerHTML = "" + usernamen;
@@ -71,12 +65,38 @@ function setzenHtml(data) {
 
 function cookies() {
   console.log(document.cookie);
-  var Wertstart = document.cookie.indexOf("=") + 1;
-  let c = document.cookie.substring(Wertstart, Wertstart + 4);
-  let z = Number(c);
-  console.log(c);
-  console.log(z);
-  return z;
+  let a = document.cookie.split(";");
+  let co = 0;
+  console.log(a);
+  console.log(a[0].substring(0, 2));
+  let result = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (i == 0 && co == 0) {
+      if (a[i].substring(0, 2) == "KN") {
+        console.log("kunde");
+        let Wertstart = a[i].indexOf("=") + 1;
+        let c = a[i].substring(Wertstart, Wertstart + 4);
+        result.push(Number(c));
+      } else {
+        console.log("kein Kunde");
+        result.push(0);
+        i--;
+      }
+      co++;
+    } else {
+      console.log("else");
+      console.log(a[i]);
+      let Wertstart = a[i].indexOf("=") + 1;
+      let c = a[i].substring(Wertstart, Wertstart + 4);
+      result.push(Number(c));
+    }
+  }
+  return result;
+}
+
+function setCookie(cname, cvalue) {
+  document.cookie = cname + "=" + cvalue + ";path=/";
 }
 
 /*

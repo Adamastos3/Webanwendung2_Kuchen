4d17f1ef6b200ed2691a5f3ac5db13c09af8d410
Adamastos3 <chseifriz3@gmail.com>
2021-04-29 16:26:10 +0200

    add setzenHtmlSR

--- frontend/public/js/sortimentR.js
+++ frontend/public/js/sortimentR.js
@@ -1,4 +1,107 @@
+//ZUm Test ist nur Produkt 13 ausgewählt
 
+const pathSR = "http://localhost:8000/wba2api/produkt/alle";
+const pathSRTest = "http://localhost:8000/wba2api/produkt/gib/13";
+var zahl = 1;
+
+function setzenHtmlSR(data) {
+  console.log(data);
+  var div = document.getElementById("textdiv");
+  console.log(div);
+  console.log("row div");
+  var text = "";
+  var id = "id" + zahl;
+  var divBId = "divB" + zahl;
+  var divAId = "divA" + zahl;
+
+  //for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i <= 0; i++) {
+    text +=
+      "<div class='containerProduct containerProductReg' id='" +
+      id +
+      "'>" +
+      "<div id='" +
+      divBId +
+      "' >" +
+      "<table class='tableProduct tableProductReg'>" +
+      //Bild
+      "<tr><td class='tableProductImage'><img src='" +
+      data.bilder[0].bildpfad +
+      "' alt=''></td></tr>" +
+      //Bezeichnung
+      "<tr><td><h4>" +
+      data.bezeichnung +
+      "</h4></td></tr>" +
+      //Beschreibung
+      "<tr><td><p>Beschreibung:</p><p>" +
+      data.beschreibung +
+      "</p></td></tr>" +
+      //Preis
+      "<tr><td><p>Preis: " +
+      data.bruttopreis +
+      "€</p></td></tr>" +
+      "</table></div>" +
+      //div2
+      "<div id='" +
+      divAId +
+      "'>" +
+      "<table class='tableProduct tableProductReg'>" +
+      //Bild
+      "<tr><td class='tableProductImage'><img src='" +
+      data.bilder[0].bildpfad +
+      "' alt=''></td></tr>" +
+      //Info + Warenkorb
+      "<tr><td><button onclick= sendOn('" +
+      data.id +
+      "')>Info</button></td></tr>" +
+      "<tr><td><button onclick=addWarenkorb('" +
+      data.id +
+      "')>In den Warenkorb</button></td></tr>" +
+      "</table></div></div>";
+
+    div.innerHTML += text;
+    console.log(div);
+    text = "";
+    zahl++;
+
+    document
+      .getElementById(id)
+      .setAttribute(
+        "onmouseover",
+        "replaceDivInvert('" + divBId + "', '" + divAId + "')"
+      );
+    document
+      .getElementById(id)
+      .setAttribute(
+        "onmouseout",
+        "replaceDiv('" + divBId + "', '" + divAId + "')"
+      );
+    replaceDivInvert(divBId, divAId);
+  }
+}
+
+function replaceDiv(a, b) {
+  document.getElementById(b).style.display = "none";
+  document.getElementById(a).style.display = "flex";
+}
+
+function replaceDivInvert(a, b) {
+  document.getElementById(b).style.display = "flex";
+  document.getElementById(a).style.display = "none";
+}
+
+function sendOn(id) {
+  console.log();
+  setCookie("Pid", id);
+  location.href = "/produkt";
+}
+
+//fehlt noch
+function addWarenkorb(id) {}
+
+getRequest(pathSRTest, setzenHtmlSR);
+
+/*
 
 function store(a) {
     let elem = a.value;
@@ -16,11 +119,7 @@ function store(a) {
     
 }
 store("test")
-
-
-
-
-
+*/
 
 //Später einsetzen
 /*

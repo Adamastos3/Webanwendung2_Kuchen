94107f3c5f0fc53f21a7296707b7deeff351bbb2
Adamastos3 <chseifriz3@gmail.com>
2021-05-06 20:45:31 +0200

    add auth

--- frontend/API_Access/Benutzer/benutzer.js
+++ frontend/API_Access/Benutzer/benutzer.js
@@ -2,33 +2,37 @@ const getRequest = require("./../Request/getRequest");
 const postRequest = require("./../Request/postRequest");
 const putRequest = require("./../Request/putRequest");
 
+const pas = "/6IyJY6Ri18lhIgNvT-_ec.zJfXz3bkEKnan0zEy_tjfUtPO~7A4nCje9GMFa";
+
 async function getBenutzerbyId(id) {
-  let pa = "/wba2api/benutzer/gib/" + id;
+  let pa = "/wba2api/benutzer/gib/" + id + pas;
   const b = await getRequest(pa);
+  console.log(b);
   return b;
 }
 
 async function getBenutzerAll() {
-  let pa = "/wba2api/benutzer/all";
+  let pa = "/wba2api/benutzer/alle" + pas;
   const b = await getRequest(pa);
   return b;
 }
 
-
 async function createBenutzer(data) {
-  let pa = "/wba2api/benutzer";
+  let pa = "/wba2api/benutzer" + pas;
   const b = await postRequest(pa, data);
+  console.log(b);
   return b.id;
 }
 
 async function checkBenutzer(data) {
-  let pa = "/wba2api/benutzer/eindeutig";
+  let pa = "/wba2api/benutzer/eindeutig" + pas;
   const b = await getRequest(pa, data);
-  return b.eindeutig;
+  console.log(b);
+  return b.daten.eindeutig;
 }
 
 async function checkBenutzerUndPassword(data) {
-  let pa = "/wba2api/benutzer/zugang";
+  let pa = "/wba2api/benutzer/zugang" + pas;
   const b = await getRequest(pa, data);
   console.log(b);
   if (b == null) {
@@ -39,7 +43,7 @@ async function checkBenutzerUndPassword(data) {
 }
 
 async function updateBenutzer(data) {
-  let pa = "/wba2api/benutzer";
+  let pa = "/wba2api/benutzer" + pas;
   const b = await putRequest(pa, data);
   console.log(b);
   return b.id;
--- frontend/API_Access/Person/person.js
+++ frontend/API_Access/Person/person.js
@@ -2,14 +2,16 @@ const getRequest = require("./../Request/getRequest");
 const postRequest = require("./../Request/postRequest");
 const putRequest = require("./../Request/putRequest");
 
+const auth = "/6IyJY6Ri18lhIgNvT-_ec.zJfXz3bkEKnan0zEy_tjfUtPO~7A4nCje9GMFa";
+
 async function getPersonbyId(id) {
-  let pa = "/wba2api/person/gib/" + id;
+  let pa = "/wba2api/person/gib/" + id + auth;
   const b = await getRequest(pa);
   return b;
 }
 
-async function getPersonnAll(id){
-  let pa = "/wba2api/person/alle";
+async function getPersonnAll(id) {
+  let pa = "/wba2api/person/alle" + auth;
   const b = await getRequest(pa);
   return b;
 }
@@ -17,13 +19,13 @@ async function getPersonnAll(id){
 async function createPerson(data) {
   //console.log("start");
   //console.log(data);
-  let pa = "/wba2api/person";
+  let pa = "/wba2api/person" + auth;
   const b = await postRequest(pa, data);
   return b.id;
 }
 
 async function updatePerson(data) {
-  let pa = "/wba2api/person";
+  let pa = "/wba2api/person" + auth;
   const b = await putRequest(pa, data);
   return b.id;
 }

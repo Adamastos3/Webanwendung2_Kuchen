304bab7e68f2e913786ea1b4dcc289f8a930b032
Adamastos3 <chseifriz3@gmail.com>
2021-05-10 21:02:42 +0200

    formatted

--- frontend/public/js/passwordVergessen.js
+++ frontend/public/js/passwordVergessen.js
@@ -1,15 +1,29 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
 //prototyp
-form.addEventListener("submit", (e)=>{
-    
-    e.preventDefault();
-    submitten()
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-function submitten(){
-    document.form.form.submit();
+function submitten() {
+  let path = "http://localhost:3000/passwordVergessen";
+  console.log("submit");
+  let data = JSON.stringify({
+    username: document.getElementById("username").value,
+    email: document.getElementById("email").value,
+  });
+  postRequest(path, data, setInfo);
 }
 
-
-
+function setInfo(data) {
+  console.log(data);
+  if (data.fehler == null) {
+    alert("Eine Mail mit dem Passwort wurde gesendet");
+  } else {
+    let text = "";
+    for (let i = 0; i < data.fehler.length; i++) {
+      text += data.fehler[i].bezeichnung + "\n";
+    }
+    alert(text);
+  }
+}

2974c6c44ca7d52e0288fbfca662230e4a9bdf37
Adamastos3 <chseifriz3@gmail.com>
2021-05-10 20:59:26 +0200

    formatted

--- frontend/Module/passwordGenerator/passwordGenerator.js
+++ frontend/Module/passwordGenerator/passwordGenerator.js
@@ -1,11 +1,12 @@
 function generatePassword() {
-    var length = 8;
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.";
-    var retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+  var length = 8;
+  var charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.";
+  var retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
 }
 
-module.exports=generatePassword;
+module.exports = { generatePassword };

b5ec7dabf5a019260719faf8123a44dfbaca3f5d
Adamastos3 <chseifriz3@gmail.com>
2021-05-10 21:00:55 +0200

    change erledigt change setzenHtml

--- frontend/public/js/ausstehendeBestellungen.js
+++ frontend/public/js/ausstehendeBestellungen.js
@@ -1,64 +1,72 @@
-var counter = 1;
 const pathAussthendeBestellung =
   "http://localhost:3000/ausstehendeBestellungen/api";
 
 function setzenHtmlAusstehendeBestellung(data) {
+  console.log(data[0]);
   let table = document.getElementById("tableAus");
   let text = "";
 
   for (let i = 0; i < data.length; i++) {
-    if (data[i].status == 0) {
-      let elem = "elem" + counter;
-      text = "";
+    let elem = data[i].id;
+    text = "";
 
-      //Bestellid
-      text += "<td><p>" + data.id + "</p></td>" + "<td>";
+    //Bestellid
+    text +=
+      "<tr id='" + elem + "'> <td><p>" + data[i].id + "</p></td>" + "<td>";
 
-      //Bestellposition
-      for (let j = 0; j < data[i].bestellpositionen.length; j++) {
-        test +=
-          "<p>" +
-          data[i].bestellpositionen.menge +
-          "x " +
-          data[i].bestellpositionen.produkt.bezeichnung +
-          " (Id: " +
-          data[i].bestellpositionen.produkt.id +
-          "</p>";
-      }
-
-      //Rechnungsbetrag
+    //Bestellposition
+    for (let j = 0; j < data[i].bestellpositionen.length; j++) {
       text +=
-        "</td>" +
-        "<td><p>" +
-        data.total.brutto +
-        " €</p></td>" +
-        //Bestellzeitpunkt
-        "<td><p>" +
-        data.bestellzeitpunkt.substring(0, 10) +
-        "</p></td>" +
-        "<td>" +
-        "<button onclick='erledigt('" +
-        elem +
-        "')'>" +
-        "<img src='./img/confirm.png' alt='' />" +
-        "</button>" +
-        "</td>";
-
-      let rw = table.insertRow(counter);
-      rw.setAttribute("id", elem);
-      rw.innerHTML += text;
-      counter++;
+        "<p>" +
+        data[i].bestellpositionen[j].menge +
+        "x " +
+        data[i].bestellpositionen[j].produkt.bezeichnung +
+        " (Id: " +
+        data[i].bestellpositionen[j].produkt.id +
+        " )</p>";
     }
+
+    //Rechnungsbetrag
+    text +=
+      "</td>" +
+      "<td><p>" +
+      data[i].total.brutto +
+      " €</p></td>" +
+      //Bestellzeitpunkt
+      "<td><p>" +
+      data[i].bestellzeitpunkt.substring(0, 10) +
+      "</p></td>" +
+      "<td>" +
+      "<button onclick=erledigt('" +
+      elem +
+      "')>" +
+      "<img src='./img/confirm.png' alt='' />" +
+      "</button>" +
+      "</td> </tr>";
+
+    table.innerHTML += text;
   }
 }
 
 function erledigt(id) {
   console.log(id);
+  let path = "http://localhost:3000/ausstehendeBestellungen";
   let element = document.getElementById(id);
-
   //console.log(element)
   element.parentNode.removeChild(element);
-  //Es fehl noch das entfernen aus dem sessionstore
+  let data = JSON.stringify({
+    id: id,
+  });
+  postRequest(path, data, showInfo);
+}
+
+function showInfo(data) {
+  console.log(data);
+  if (data.fehler == null) {
+    alert("Bestellung ist erledigt");
+  } else {
+    alert(data.fehler[0].bezeichnung);
+  }
 }
 
 //ausführen

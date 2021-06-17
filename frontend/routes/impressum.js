f6e2cb45f5cfc3ab64c7258142b4e1dbabd5d972
Adamastos3 <chseifriz3@gmail.com>
2021-06-17 02:09:00 +0200

    add route for AGB

--- frontend/routes/impressum.js
+++ frontend/routes/impressum.js
@@ -1,13 +1,19 @@
-const path= require('path')
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
-const server =express()
-
+const server = express();
 
 //impressum
-server.get("/impressum", (req,res) =>{
-    res.sendFile('impressum.html', { root: path.join(__dirname,'..', 'view') });
+server.get("/impressum", (req, res) => {
+  res.sendFile("impressum.html", { root: path.join(__dirname, "..", "view") });
 });
 
+server.get("/impressum/agb", (req, res) => {
+  let tempFile = path.join(__dirname, "../public/pdf", "Rechtstext_AGB.pdf");
+  fs.readFile(tempFile, function (err, data) {
+    res.contentType("application/pdf");
+    res.send(data);
+  });
+});
 
-
-module.exports=server;
+module.exports = server;
